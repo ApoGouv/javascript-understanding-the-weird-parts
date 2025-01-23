045778f0ae1c20bc84f8dc58720a5709cbedf5b7
ApostolosG <apostolosg@mentalmedia.gr>
2025-01-23 13:11:41 +0200

    2.11 The Scope Chain

--- 02_Execution_Contexts/ScratchPad/app.js
+++ 02_Execution_Contexts/ScratchPad/app.js
@@ -1,13 +1,38 @@
 function b() {
-  var myVar;
-  console.log(myVar);
+  console.log(myVar); // 1
 }
+
 function a() {
   var myVar = 2;
-  console.log(myVar);
   b();
 }
+
 var myVar = 1;
-console.log(myVar);
 a();
-console.log(myVar);
+
+/*
+function a() {
+  function b() {
+    console.log(myVar); // 2
+  }
+
+  var myVar = 2;
+  b();
+}
+
+var myVar = 1;
+a();
+*/
+
+/*
+function a() {
+  function b() {
+    console.log(myVar); // 1
+  }
+
+  b();
+}
+
+var myVar = 1;
+a();
+*/
